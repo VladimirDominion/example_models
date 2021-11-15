287fe8e0d103b6cb937c6157ad18aa830580270f
vladimir <volodymyr.dominion@myntelligence.com>
2021-11-15 17:45:33 +0200

    Change database settings

---
+++ README.md
@@ -0,0 +1,2 @@
+# Create Django Project
+
--- pizza/models.py
+++ pizza/models.py
@@ -2,7 +2,8 @@ from django.db import models
 
 
 class Topping(models.Model):
-    name = models.CharField(max_length=200)
+    name = models.CharField(max_length=200, default='')
+    size = models.IntegerField(null=True, blank=True, default=0)
 
     def __str__(self):
         return self.name
